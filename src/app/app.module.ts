d487a077fd4cd80466221ea9b6cf86ed9a54dc8f
Johnny Rockembach Kraemer <johnny@alunos.utfpr.edu.br>
2021-11-13 16:53:44 -0300

    Primeira versão footer; Ajustes no header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/public/home/home.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
