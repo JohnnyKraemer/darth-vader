6894581db0b9f13095f96c64b87680b8ac2cb7fd
Johnny Rockembach Kraemer <johnny@alunos.utfpr.edu.br>
2021-11-13 14:30:52 -0300

    Criação do home component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/public/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: '/admin',
+  //   pathMatch: 'full',
+  // },
+  { path: '', component: HomeComponent },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
