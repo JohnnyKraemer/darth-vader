34a09eb605c6dee047b5f1ad31c78b9e9cd5a934
Johnny Rockembach Kraemer <johnny@alunos.utfpr.edu.br>
2021-11-15 08:08:12 -0300

    Reestruturação da página home.

--- src/app/views/public/home/home.component.ts
+++ src/app/views/public/home/home.component.ts
@@ -4,7 +4,8 @@ import { Comic } from 'src/app/core/models/comic.model';
 import { Serie } from 'src/app/core/models/serie.model';
 import { MarvelService } from 'src/app/core/services/marvel.service';
 
-const FAVORITES = ['19379','30146','29329','24396','1067','20615','19242','1058', '16593']
+const FAVORITES_STAR_WARS = ['19379','19242','29291','21131','19486','20659', '73000']
+const FAVORITES = ['19379','19242','29329','24396','1067','20615','19242','1058', '16593']
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,7 +13,7 @@ const FAVORITES = ['19379','30146','29329','24396','1067','20615','19242','1058'
 })
 export class HomeComponent implements OnInit {
   public series: Serie[] = [];
-  public favorites = FAVORITES;
+  public favorites = FAVORITES_STAR_WARS;
 
   constructor(private _marvelService: MarvelService) {}
 
